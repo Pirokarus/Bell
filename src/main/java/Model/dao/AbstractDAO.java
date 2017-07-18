808ce942df6c69d6f2221c8de5467a5f484546bd
Pirokarus <olimjon.kosymoff@yandex.ru>
2017-07-18 10:34:50 +0300

    1

--- src/main/java/Model/dao/AbstractDAO.java
+++
@@ -1,8 +0,0 @@
-package main.java.model.dao;
-
-import main.java.model.data.Base;
-
-public interface AbstractDAO {          //Интерфейс с основными функциями всех будущих возможных DAO
-    void saveBase(Base base);
-    Base getBase();
-}
--- src/main/java/exceptions/MyNotPhoneNumberException.java
+++
@@ -1,4 +0,0 @@
-package main.java.exceptions;
-
-public class MyNotPhoneNumberException extends Exception {    //Моё исключение для неправильных символов в номере
-}
